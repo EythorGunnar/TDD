cf3e5c9487a9d4e05d712db763a94f00f6639ec2
Eythor <eythor09>
2015-10-17 21:18:35 +0000

    Code can add more than one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,8 +6,15 @@ public class Calculator {
 		if (text == ("")) {
 			return 0;
 		}
-		else
-			return 1;
+		else if (text.contains(",")) {
+			String [] allTheNumbers = text.split(",");
+			int sum = 0;
+			for (int x = 0; x < allTheNumbers.length; x++) {
+				sum += Integer.parseInt(allTheNumbers[x]);
+			}
+			return sum;
+		}
+		else;
+			return Integer.parseInt(text);
 	}
-
 }
